a62ab50f9d3a1ac5bcc59ee241e32adadc73f20b
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-09-08 13:10:02 -1000

    complete

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const gridContainer = document.querySelector('.gridContainer');
 const clrBtn = document.querySelector('.clear');
+const rnbwBtn = document.querySelector('.rainbow');
+const blkBtn = document.querySelector('.black');
+const gridBtn = document.querySelector('.size');
+
 let numberOfRows = 16
 
 let containerSize = gridContainer.clientWidth;
@@ -9,6 +13,7 @@ function createGrid(rows) {
         for (j = 0; j < rows; j++) {
             let cells = document.createElement('div');
             cells.classList = "cells";
+            gridContainer.setAttribute('style', `display: inline-grid; grid-template-columns: repeat(${rows}, 2fr); grid-template-rows: repeat(${rows}, 2fr);`);
             gridContainer.appendChild(cells);
             cells.addEventListener('mouseover', function(event){
                 event.target.style.backgroundColor = 'black';
@@ -16,6 +21,9 @@ function createGrid(rows) {
         }
     }
 }
+    
+gridBtn.addEventListener('click', resetGrid);
+    
 
 // clears all the colors on the grid
 clrBtn.addEventListener('click', () =>{
@@ -27,6 +35,48 @@ function clearGrid(rows) {
     for (let i = 0; i < rows * rows; i++) {
         cells[i].style.backgroundColor = 'white';
     }
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+rnbwBtn.addEventListener('click', () => {
+    let cells = gridContainer.children;
+    for (let i = 0; i < numberOfRows * numberOfRows; i++) {
+        cells[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = getRandomColor();
+        })
+    }
+})
+
+blkBtn.addEventListener('click', () => {
+    let cells = gridContainer.children;
+    for (let i = 0; i < numberOfRows * numberOfRows; i++) {
+        cells[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+    }
+})
+
+function resetGrid(){
+    numberOfRows = prompt("Enter Grid Size:                     1 to 99 only")
+    if (numberOfRows < 1 || numberOfRows > 99) {
+        alert('invalid input!');
+        gridContainer.innerHTML = '';
+        createGrid(16);
+    } else {
+        console.log(numberOfRows);
+        gridContainer.innerHTML = '';
+        createGrid(numberOfRows);
+    }
+
+    
 }
 
 createGrid(numberOfRows);
