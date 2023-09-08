08e246faa607f9476e26e35946890e217b8eedf6
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-09-08 09:23:47 -1000

    added hover effect in JS

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const gridContainer = document.querySelector('.gridContainer');
+const clrBtn = document.querySelector('.clear');
 let numberOfRows = 16
 
 let containerSize = gridContainer.clientWidth;
-let cellSize = 400 / numberOfRows;
-
-
-
+// creates a 16x16 grid
 function createGrid(rows) {
     for (i = 0; i < rows; i++) {
         for (j = 0; j < rows; j++) {
             let cells = document.createElement('div');
             cells.classList = "cells";
-            cells.setAttribute('style', `width: ${cellSize}px;`);
             gridContainer.appendChild(cells);
-            
+            cells.addEventListener('mouseover', function(event){
+                event.target.style.backgroundColor = 'black';
+            })
         }
     }
 }
 
-console.log(squares);
+// clears all the colors on the grid
+clrBtn.addEventListener('click', () =>{
+    clearGrid(numberOfRows);
+})
+function clearGrid(rows) {
+    
+    let cells = gridContainer.children;
+    for (let i = 0; i < rows * rows; i++) {
+        cells[i].style.backgroundColor = 'white';
+    }
+}
+
 createGrid(numberOfRows);
